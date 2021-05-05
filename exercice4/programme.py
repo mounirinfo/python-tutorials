fa36ab4313293a4ab4836f2128ab98246e700392
Nabila <bila.akrour@gmail.com>
2021-05-05 20:39:53 +0200

    programme

--- exercice4/programme.py
+++ exercice4/programme.py
@@ -1,4 +1,3 @@
-import xlrd
 
 import pandas as pd 
 
@@ -9,24 +8,28 @@ liste=nom_fichier.split('.')
 print('chaine',liste)
 format_fichier=liste[-1]
 
-if (format_fichier) =='xlsx' :
+if format_fichier =='xlsx' :
     
     data = pd.read_excel(nom_fichier, sheet_name='Feuil1')
-    print(data)
-    
-    
-    # wb = xlrd.open_workbook(nom_fichier)
-    # sheet = wb.sheet_by_index(0)
-    
-    # print(sheet.cell_value(0, 0))
-    
-    
-    #afficher le nombre de ligne dans le fichier 
-    # n_ligne=sheet.nrows
-    # for i in range(sheet.nrows):
-    #     somme= (sheet.cell_value(i, 3))
-    #     print("la moyenne d'age est: ",somme)
-    
+    print (' Menu des actions a faire :')
+    print('+++++++++++++++++++++++++++++')
+    print ( '1- Afficher les colonnes du fichier')
+    print ( '2- Le nombre de lignes de fichier')
+    print(' 3- Exporter les 10 premieres lignes dans un autre fichier excel')
+    print (' 4- exporter un fichier avec les colonnes et la moyenne de chaque colonne si elle est numérique')
+    print('++++++++++++++++++++++')
+    numero= input ('choisir le numero de laction que vous voulez faire ' )
+    if numero =='1':
+        print('les colonnes du fichier', data.columns)
+    elif numero==2:
+        print('le nombre de lignes de fichier')
+    elif numero==3:
+        print('lexportation des 10 premeires lignes' )
+    elif numero==4:
+        print('la moyenne de chaque colonne')
+    else:
+        print('resaissez le bon numero')
+
 else:
-    print ("le format du ficheir est erronne ou le fichier n'existe pas")
+    print ("le format du fichier est erroné ou le fichier n'existe pas")
    
--- exercice4/readme.txt
+++ exercice4/readme.txt
@@ -1,8 +1,14 @@
-dans le repository tutoriels( créé avec abdou), ajouter un dossier dan slequel il ya un programme python, quand je l’exécute, il me demande de choisir un nom de fichier
-l’utilise choisit un fichier excel (gérer les erreurs , fihcier introuvalbe, format erronée ...etc)
-charger le fichier danas un DF
-afficher àlutilisateur un menu numéroté, selon ce que saiis l’utilisateur comme numéro il peut avoir une action,.
-par exemple s’il saisit 1, il peu tafficher le colonnes du fichier
+dans le repository tutoriels( créé avec abdou), 
+ajouter un dossier dan slequel il ya un programme python, quand je l’exécute,
+ il me demande de choisir un nom de fichier
+l’utilisateur choisit un fichier excel (gérer les erreurs , 
+fihcier introuvalbe, format erronée ...etc)
+charger le fichier dans un DF
+
+afficher àlutilisateur un menu numéroté,
+ selon ce que saiis l’utilisateur comme numéro il peut avoir une action,.
+par exemple:
+ s’il saisit 1, il peu tafficher le colonnes du fichier
 2 le nombre de lignes du fichier
-3exporter les 10 premières lignes sous un autre fichier excel
-4exporter un fichier avec les colonnes et la moyenne de chaque colonne si elle est numérique
+3 exporter les 10 premières lignes sous un autre fichier excel
+4 exporter un fichier avec les colonnes et la moyenne de chaque colonne si elle est numérique
