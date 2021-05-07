0ed4eeb1251f50190be2c434d4f0c53e43d3e3a8
Nabila <bila.akrour@gmail.com>
2021-05-07 14:38:36 +0200

    traitement de donnees de vente de produits pharmaceutique

--- exercice4.1/gestion_med.py
+++ exercice4.1/gestion_med.py
@@ -1,6 +1,6 @@
 import pandas as pd
 data = pd.read_excel('test_vente.xlsx', sheet_name='Feuil1')
-
+data['chiffre_daffaires']=data.quantite*data.prix_unit
 condition = True
 
 print(" le menu des informations disponibles ")
@@ -32,55 +32,45 @@ while condition:
         if selection =='1':
             maxe=data["quantite"].argmax()
             print("Le medicament le plus vendu: ", data.loc[maxe,:])
-            afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
-            if afficher_menu == '1':
-                condition = False
-                print("vous avez choisi de quitter le menu, au revoir!!!")
         elif selection =='2':
             mine=data["quantite"].argmin()
             print("Le medicament le moins  vendu: ", data.loc[mine,:])
-            afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
-            if afficher_menu == '1':
-                condition = False
-                print("vous avez choisi de quitter le menu, au revoir!!!")
         elif selection =='3':
             somme=data["quantite"].sum()
             print("le nombre total des produits vendus", somme)
-            afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
-            if afficher_menu == '1':
-                condition = False
-                print("vous avez choisi de quitter le menu, au revoir!!!")
         elif selection =='4':
             nombre_produit=data.shape[0]
             print("le nombre de references de produits vendus", nombre_produit)
-            afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
-            if afficher_menu == '1':
-                condition = False
-                print("vous avez choisi de quitter le menu, au revoir!!!")
         elif selection =='5':
             tabl=data[['id_client','quantite']].groupby(['id_client']).sum('quantite')
             print("produits vendus par client",tabl)
             nombre_client= len (pd.unique(data['id_client']))
             print("le nombre de client est: " , nombre_client)
-            afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
-            if afficher_menu == '1':
-                condition = False
-                print("vous avez choisi de quitter le menu, au revoir!!!")
         elif selection =='6':
-            print("ok")
+            ca_global=data['chiffre_daffaires'].sum()
+            print("Le chiffre daffaires realise est: ", ca_global)
         elif selection =='7':
-            print("ok")
+           med_vendu_max=data['chiffre_daffaires'].argmax()
+           print("Le medicament qui a realise le plus grand chiffre daffaires est: ", data.loc[med_vendu_max,:])
         elif selection =='8':
-            print("ok")
+            med_vendu_min=data['chiffre_daffaires'].argmin()
+            print("Le medicament qui a realise le plus moins chiffre daffaires est: ", data.loc[med_vendu_min,:])
         elif selection =='9':
-            print("ok")
+             client_vendu_max=data['chiffre_daffaires'].argmax()
+             print("le client ayant genere plus grand chiffre daffaires", data.iloc[client_vendu_max]['id_client'])
         elif selection =='10':
-            print("ok")
+            client_acheter_max_produits=data['quantite'].argmax()
+            print("le client ayant achete le plus de produits", data.iloc[client_acheter_max_produits]['id_client'])
         elif selection == '0':
             print("Exit")
             condition = False
+        afficher_menu= input("voulez vous reafficher le menu? saisir:  \n 0--> Oui \n 1--> Non : \n")
+        if afficher_menu == '1':
+            condition = False
+            print("vous avez choisi de quitter le menu, au revoir!!!")    
+    
     else:
         print("Votre choix n'est pas dans la liste, veuillez choisir une option de la liste suivante :\n")
-        
+    
     
         
