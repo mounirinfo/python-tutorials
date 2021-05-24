26cd4e1d891e54b7a0225dcdeef78e34783feb31
Nabila <bila.akrour@gmail.com>
2021-05-24 13:29:53 +0200

    explications

--- traitement_donnees/traitement_donnees.py
+++ traitement_donnees/traitement_donnees.py
@@ -1,6 +1,7 @@
 import pandas as pd
 data_frame_melb=pd.read_csv("melb_data.csv")
 data_frame_sans_case_vide=data_frame_melb.dropna() # pourquoi tu as supprimé les na  ? il y a combien de na qui sont supprimés après cette ligne
+# j'ai opté pour la suppression des lignes des données manquantes, qui sont a peu pres d'une moitié des données brutes pour cette raison: je ne pourrai pas appliquer une methode pour prédire la valeur de chaque colonne manquante neanmoins je peux proceder peut etre a la suppression des colonnes qui ont beaucoup de valeurs manquantes. mais derriere le choix de la methode pour  traitmeent des donnees revient a ce que on veut reellement savoir  
 print (data_frame_sans_case_vide.info())
 #print(data_frame_sans_case_vide["Suburb"].value_counts())
 #frame_suburb=pd.get_dummies(data_frame_sans_case_vide["Suburb"])
@@ -12,5 +13,7 @@ print (data_frame_sans_case_vide.info())
 #frame_sellerg=pd.get_dummies(data_frame_sans_case_vide["SellerG"])
 data_frame_sans_case_vide['Date']=pd.to_datetime(data_frame_sans_case_vide['Date'])
 #pourquoi tu as utilisé les prefixes? et c'est quoi le résultat du get_dummies
+
+# j'ai utilisé les prefixes pour voir la nouvelle colonne ajoutee elle etait reellement la valeur de la variable de quelle du dataframe initial  
 data_exp=pd.get_dummies(data_frame_sans_case_vide, prefix = ['Suburb','Address','Type','Method','sellerG','CouncilArea','Regionname'], columns=['Suburb','Address','Type','Method','SellerG','CouncilArea','Regionname'])
 print(data_exp)
