044bc9cfa8c316a6e8aa6b6358019e5128cb6360
Mounir ZAGHBA <zaghba.mounir@gmail.com>
2021-05-24 13:13:59 +0200

    add questions

--- traitement_donnees/traitement_donnees.py
+++ traitement_donnees/traitement_donnees.py
@@ -1,6 +1,6 @@
 import pandas as pd
 data_frame_melb=pd.read_csv("melb_data.csv")
-data_frame_sans_case_vide=data_frame_melb.dropna()
+data_frame_sans_case_vide=data_frame_melb.dropna() # pourquoi tu as supprimé les na  ? il y a combien de na qui sont supprimés après cette ligne
 print (data_frame_sans_case_vide.info())
 #print(data_frame_sans_case_vide["Suburb"].value_counts())
 #frame_suburb=pd.get_dummies(data_frame_sans_case_vide["Suburb"])
@@ -11,6 +11,6 @@ print (data_frame_sans_case_vide.info())
 #frame_methode=pd.get_dummies(data_frame_sans_case_vide["Method"])
 #frame_sellerg=pd.get_dummies(data_frame_sans_case_vide["SellerG"])
 data_frame_sans_case_vide['Date']=pd.to_datetime(data_frame_sans_case_vide['Date'])
-
+#pourquoi tu as utilisé les prefixes? et c'est quoi le résultat du get_dummies
 data_exp=pd.get_dummies(data_frame_sans_case_vide, prefix = ['Suburb','Address','Type','Method','sellerG','CouncilArea','Regionname'], columns=['Suburb','Address','Type','Method','SellerG','CouncilArea','Regionname'])
 print(data_exp)
