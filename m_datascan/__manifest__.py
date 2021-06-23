1236d657cb708a44d57dd606e6bf84025f6781fe
name <bila.akrour@gmail.com>
2021-06-24 00:23:39 +0200

    test module odoo

---
+++ m_datascan/__manifest__.py
@@ -0,0 +1,25 @@
+
+{
+	'name' : "mdatascan",
+	'data' : [
+		'security/ir.model.access.csv',
+		'views/test_model_datascan.xml',
+    
+	],	
+	    
+    'version': '1.0',
+    'summary': 'test datascan',
+    'sequence': -100,
+    'description': """test datascan""",
+    'category': 'Productivity',
+    'website': 'www.data-scan.io',
+    'license': 'LGPL-3',
+    'depends': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+	
+} 
